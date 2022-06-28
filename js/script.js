02f6b003855d261b87ab0c5fc176cd55b2d58f50
kjw1025 <di1025p@gmail.com>
2022-06-29 01:29:28 +0900

    왼쪽상단 메뉴 js작업

--- js/script.js
+++ js/script.js
@@ -1,9 +1,56 @@
-window.onload = function(){
-  
+// 멀티미디어 요소 로딩 전 실행
+$(document).ready(function () {
+
+  // 왼쪽 상단 메뉴
+  let l_t_m_div = $('.l-t-m-div');
+
+  // 보기 버튼 기능
+  let l_t_icon = $('.l-t-icon');
+  l_t_icon.click(function () {
+    l_t_m_div.show();
+  });
+
+  // 닫기 버튼 기능
+  let close_btn = $('.close-btn');
+  close_btn.click(function () {
+    l_t_m_div.hide();
+  });
+
+  // 배경 누르면 닫기
+  l_t_m_div.click(function () {
+    l_t_m_div.hide();
+  });
+
+  // 내용을 클릭하면 배경 신호 전달 막기
+  $('.l-t-m-div-bg').click(function (event) {
+    // 신호 전달 막기
+    event.stopPropagation();
+  });
+
+
+});
 
 
 
-    // 비주얼 슬라이드
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function () {
+
+
+
+
+  // 비주얼 슬라이드
   let sw_visual = new Swiper('.sw-visual', {
     loop: true,
     speed: 1000,
@@ -15,17 +62,17 @@ window.onload = function(){
   });
 
 
-    // 베스트 아트 슬라이드
+  // 베스트 아트 슬라이드
   let sw_best_art = new Swiper('.sw-best_art', {
     slidesPerView: 4,
     spaceBetween: 10,
-    
+
     // nav
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    
+
     //pg
     pagination: {
       el: ".swiper-pagination",
@@ -33,10 +80,10 @@ window.onload = function(){
   });
 
 
-  
 
 
-    // 뉴 아트 슬라이드
+
+  // 뉴 아트 슬라이드
   let sw_new_art = new Swiper('.sw-new_art', {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -55,29 +102,31 @@ window.onload = function(){
   let sw_rev = new Swiper('.sw-rev', {
     slidesPerView: 3,
     spaceBetween: 30,
-    
+
     // nav
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  
+
     //pg
     pagination: {
       el: ".swiper-pagination",
     },
-  
+
   });
 
-  
-  
+
+
   // gotop
   let gotop_icon = $('.gotop-icon');
-  gotop_icon.click(function(){
-    $('html').animate({scrollTop:0}, 1000);
+  gotop_icon.click(function () {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
 
   });
-  
+
 
 
 }
