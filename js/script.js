89a781582ae9a7a1980f2619ee80bbac867eecfe
unknown <di1025p@gmail.com>
2022-07-07 18:09:01 +0900

    메뉴 동작 구현 작업 및 호버효과 디테일 추가

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-window.onload = function(){
-  
+window.onload = function () {
 
 
 
-    // 비주얼 슬라이드
+  // 메뉴 오픈 기능
+  $('.l-t-icon').click(function (e) {
+    e.preventDefault();
+    $('.left-menu-wrap').toggleClass('left-menu-wrap-open');
+    $('.left-menu-dim').toggleClass('left-menu-dim-open');
+  });
+
+  // 메뉴 닫기 기능
+  $('.close-btn').click(function (e) {
+    e.preventDefault();
+    $('.left-menu-wrap').removeClass('left-menu-wrap-open');
+    $('.left-menu-dim').removeClass('left-menu-dim-open');
+  });
+
+
+
+
+
+
+
+
+
+
+
+  // 비주얼 슬라이드
   let sw_visual = new Swiper('.sw-visual', {
     loop: true,
     speed: 1000,
@@ -15,17 +38,17 @@ window.onload = function(){
   });
 
 
-    // 베스트 아트 슬라이드
+  // 베스트 아트 슬라이드
   let sw_best_art = new Swiper('.sw-best_art', {
     slidesPerView: 4,
     spaceBetween: 10,
-    
+
     // nav
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    
+
     //pg
     pagination: {
       el: ".swiper-pagination",
@@ -33,10 +56,10 @@ window.onload = function(){
   });
 
 
-  
 
 
-    // 뉴 아트 슬라이드
+
+  // 뉴 아트 슬라이드
   let sw_new_art = new Swiper('.sw-new_art', {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -55,29 +78,31 @@ window.onload = function(){
   let sw_rev = new Swiper('.sw-rev', {
     slidesPerView: 3,
     spaceBetween: 30,
-    
+
     // nav
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  
+
     //pg
     pagination: {
       el: ".swiper-pagination",
     },
-  
+
   });
 
-  
-  
+
+
   // gotop
   let gotop_icon = $('.gotop-icon');
-  gotop_icon.click(function(){
-    $('html').animate({scrollTop:0}, 1000);
+  gotop_icon.click(function () {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
 
   });
-  
+
 
 
 }
