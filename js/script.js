e8639e69869abcdf9e5729538e22a028cac3e907
= <=>
2022-10-13 14:36:13 +0900

    모달창 확인

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  // modal 기능
+  let modalWrap = $(".modal-wrap");
+  let modalClose = "modal-close-active";
+  let modalCloseBt = $(".modal-close");
+
+  modalCloseBt.click(() => {
+    modalWrap.addClass(modalClose);
+  });
+
   // gotop
   let gotop_icon = $(".gotop-icon");
   gotop_icon.click(function () {
